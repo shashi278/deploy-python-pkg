2480e838d81b92903f8e311ef1a74b09dfe0f215
Shashi Ranjan <shashiranjankv@gmail.com>
2020-03-07 18:13:09 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
-python3 -m pip install --user --upgrade twine
 
 TWINE_USERNAME="$INPUT_USER" TWINE_PASSWORD="$INPUT_TOKEN" \
   exec twine upload dist/*
