0cfed9884f7f7fff64dadf142224c201900a5a98
Shashi Ranjan <shashiranjankv@gmail.com>
2020-03-07 11:10:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+python -m pip install --user --upgrade setuptools wheel
+python3 setup.py sdist bdist_wheel
+python3 -m pip install --user --upgrade twine
+
+TWINE_USERNAME="$INPUT_USERNAME"
+TWINE_PASSWORD="$INPUT_PASSWORD"
+exec twine upload dist/*
