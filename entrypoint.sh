8d011a80b4b38059df14788f730e80db2630d16b
Shashi Ranjan <shashiranjankv@gmail.com>
2020-03-07 12:20:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-python -m pip install --user --upgrade setuptools wheel
+python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 python3 -m pip install --user --upgrade twine
 
